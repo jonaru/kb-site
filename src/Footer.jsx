ed48034f958419aefd489a80cf7530bcbe40033c
Jonatan <jonatan.rugarn@gmail.com>
2025-06-17 06:40:27 +0200

    Minor style updates

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="footer">
+    <div className="footer sorts-mill-goudy">
       <ul>
         <li>© 2025 Lingonite AB</li>
         <li>Pictures used with permission from Drummonds</li>
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <div className="header-brand">
+    <div className="header-brand sorts-mill-goudy">
       <h1>Klassiska bad</h1>
       <p>
         Stockholm <span>&bull;</span> Göteborg
