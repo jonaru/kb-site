1e660b25f2f8e2094004479f1822baa4b9757fec
Jonatan <jonatan.rugarn@gmail.com>
2025-06-17 11:51:59 +0200

    styled about section

--- src/routes/index.lazy.jsx
+++ src/routes/index.lazy.jsx
@@ -11,17 +11,43 @@ function Index() {
         <h2 className="hero-heading"></h2>
         <p className="hero-text">
           Upptäck vår kollektion av viktorianska och edwardianska
-          badrumsarmaturer, noggrant tillverkade enligt högsta standard med lyx
+          badrumsarmaturer, noggrant tillverkade enligt högsta standard av lyx
           och elegans.
         </p>
       </section>
       <section className="section about">
         <h2>Om oss</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
-          quod, reiciendis eum facere alias non quae temporibus fugit aut
-          praesentium?
-        </p>
+        <div className="about-content">
+          <img
+            className="about-img"
+            src="./img/castle.jpg"
+            alt="Picture of a castle"
+          />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis optio
+            praesentium aperiam id doloremque consectetur est quibusdam, vitae
+            architecto harum? Quae id autem non quidem maxime ea. Aliquam labore
+            quae soluta et magnam, animi deleniti deserunt perferendis pariatur
+            odio asperiores tempore earum? Ipsa asperiores sint ratione nemo
+            deserunt officia facilis incidunt at adipisci, maxime mollitia. Ea
+            rerum libero consectetur, laborum rem omnis beatae similique impedit
+            fugiat quas recusandae nemo debitis maxime quae enim a id corporis
+            quisquam eaque? Eveniet aperiam labore at quis tenetur beatae
+            praesentium officiis vitae quaerat perferendis veritatis, iure unde
+            non modi minus quo nobis exercitationem cum eius iste. Error non
+            sint qui nostrum reiciendis sit voluptas repellendus consequatur
+            aspernatur labore. Laborum nisi totam perferendis placeat animi,
+            quis doloribus dicta optio, culpa rem deleniti architecto esse
+            reiciendis repudiandae, maiores dolore soluta vitae illo ea autem
+            sapiente? Aliquid ut dolore accusamus voluptatem, excepturi adipisci
+            cumque atque minima dolorem dicta incidunt libero molestiae unde in
+            aliquam iure natus ad eum rerum praesentium animi! Accusamus qui
+            fugit vel quas voluptatem velit, laudantium suscipit. Vel, odio.
+            Dolor, amet rem temporibus qui, explicabo a atque sed provident ab
+            debitis eveniet autem laboriosam modi? Incidunt, provident harum
+            omnis animi illum possimus eaque dolor!
+          </p>
+        </div>
       </section>
     </div>
   );
