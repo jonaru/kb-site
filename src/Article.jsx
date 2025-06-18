586fd3f1fdc4a82288b3f579a0243915d17d8f7d
Jonatan <jonatan.rugarn@gmail.com>
2025-06-18 08:27:09 +0200

    Fixed effects on article items and refactored css

--- src/Article.jsx
+++ src/Article.jsx
@@ -1,25 +1,13 @@
+import "../styles/Article.css";
 import { getImageURL } from "./utils/image-utils";
 
 export default function Article(props) {
   return (
     <div className="article-item">
       <img src={getImageURL(props.fileName)} alt={props.description} />
+      <div className="article-text-wrapper">
+        <p className="article-image-text">{props.description}</p>
+      </div>
     </div>
   );
 }
-
-// export default function Article(props) {
-//   const { loading, error, image } = useImage(props.fileName);
-
-//   if (error) return <p>{props.description}</p>;
-
-//   return (
-//     <div className="article-item">
-//       {loading ? (
-//         <p>Loading...</p>
-//       ) : (
-//         <img src={image} alt={props.description} />
-//       )}
-//     </div>
-//   );
-// }
